6e3faf159e294b113f92b00b742dcfb2f4e1b634
Jakob Skjerning <jakob@mentalized.net>
2014-12-16 14:27:43 +0100

    Cleanup

--- scripts/keyboard_event.js
+++
@@ -1,9 +0,0 @@
-function doKeyDown(evt){
-    switch (evt.keyCode) {
-        case 13:  /* enter key was pressed */
-            $('.test').hide();
-            break;
-    }
-}
-
-window.addEventListener('keydown',doKeyDown,true); //eventlistener, key input
